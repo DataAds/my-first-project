c0b26383b7ca328594887fdc9704553fd97af518
DataAds <leonidr131197@gmail.com>
2025-08-24 11:39:52 +0300

    Update App.tsx
    ТЕСТ КНОПКА

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,6 +1,18 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 
+import { Button } from "@/components/ui/Button";
+
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <div>
+      <h1>DataAds App</h1>
+      <Button onClick={() => alert("Кнопка работает!")}>
+        Нажми меня
+      </Button>
+
+      {/* Здесь твой роутер */}
+      <RouterProvider router={router} />
+    </div>
+  );
 }
